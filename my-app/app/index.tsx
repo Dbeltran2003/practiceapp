76603f3223ceec72d57d1fd301bbc6888bca73b8
Dbeltran2003 <dasanbelqui@gmail.com>
2025-09-24 18:12:37 -0500

    added button and text input to be able to start a list

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.intputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal!' />
+        <Button title='Add Goal' onPress={() => {}} />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+         </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  appContainer: {
+    padding : 50
+  },
+  intputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc', 
+    width: '70%',      
+    marginRight: 8, 
+    padding: 8
+    },
+
+});
