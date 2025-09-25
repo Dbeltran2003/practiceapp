a80f06e924f6f08304c2dc393750897c43003502
Dbeltran2003 <dasanbelqui@gmail.com>
2025-09-24 19:46:20 -0500

    added 2 function and 3 const also imported useState to make the list a nd to be able to add text to it from the text box

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,27 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState<string[]>([]);
+  function goalInputHandler(enteredText: string) {
+    setEnteredGoalText(enteredText);
+  }
+  
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, enteredGoalText]);
+    setEnteredGoalText('');
+  }
+  
   return (
     <View style={styles.appContainer}>
       <View style={styles.intputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!' />
-        <Button title='Add Goal' onPress={() => {}} />
+        <TextInput style={styles.textInput} 
+        placeholder='Your course goal!' 
+        onChangeText={goalInputHandler}/>
+        <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
-      <View>
-        <Text>List of goals...</Text>
-         </View>
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding : 50
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1
   },
   intputContainer: {
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc' 
   },
   textInput: {
     borderWidth: 1,
@@ -30,5 +50,8 @@ const styles = StyleSheet.create({
     marginRight: 8, 
     padding: 8
     },
+    goalsContainer: {
+      flex: 5
+    }
 
 });
